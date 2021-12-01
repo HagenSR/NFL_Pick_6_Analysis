eb0b13e9b5872f8aac56407ff622b01d56aadaad
Sean Hagen <sean.r.hagen@gmail.com>
2021-11-30 19:44:25 -0600

    Voting classifier

--- analysis/voting_classifier.py
+++ analysis/voting_classifier.py
@@ -24,8 +24,11 @@ class NaiveBayesAnalysis:
 
         self.df = self.df.drop("score_bin", axis=1)
 
-        # list of all feature indicies
-        self.features = [self.df.columns[i] for i in range(len(self.df.columns))]
+        self.features = []
+        self.features.append(['schedule_week', 'spread_favorite', 'home_win', 'home_loss'])
+        self.features.append(['spread_favorite', 'home_win', 'home_loss', 'away_win'])
+        self.features.append(['schedule_week', 'team_home', 'spread_favorite', 'home_win', 'home_loss', 'away_win', 'away_loss'])
+        self.features.append(['schedule_season','schedule_week','team_home','team_away','team_favorite_id','spread_favorite','over_under_line','stadium','home_win','home_loss','home_tie','away_win','away_loss','away_tie'])
 
         # Random state seed, results dict
         self.random_state = 101
@@ -42,13 +45,26 @@ class NaiveBayesAnalysis:
 
         # Set up kfold validation
         folds = KFold(n_splits=5, random_state=self.random_state, shuffle=True)
+        incorrect = 0
+        total = 0
+        for index in range(len(self.features)):
+            combo = self.features[index]
+            if index % 1000 == 0:
+                print("Start: {0}, Index {1} out of {2}".format(datetime.now().time(), index, len(self.features)))
+            incorrect = 0
+            total = 0
 
-        for clf, label in zip([gnb, tree, kneigh, vote], ['Naive Bayes', 'MLP', 'Kneigh', 'Vote']):
-            scores = cross_val_score(clf, self.df, self.target_names, scoring='accuracy', cv=5)
-            print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+            # run the NB model over each kfold
+            for train_index, test_index in folds.split(self.df):
+                X_train, X_test = self.df.loc[train_index][list(combo)], self.df.loc[test_index][list(combo)]
+                y_gnb = vote.fit(X_train, self.target_names[train_index]).predict(X_test)
+
+                incorrect += (self.target_names[test_index] != y_gnb ).sum()
+                total += len(y_gnb)
 
             # Add results to total for this feature combo
-            self.results[str(label)] = {"accuracy" : scores.mean(), "stdev": scores.std()}
+            self.results[str(combo)] = {"correct" : str(total - incorrect), "incorrect" : str(incorrect), 
+                                    "total": str(total) , "accuracy" : (total- incorrect) / total }
 
     def generate_matrix(self):
         gnb = GaussianNB()
